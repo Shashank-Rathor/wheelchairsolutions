b9a66b96c35458c16e397d1a03e5196b54a3a4d4
Shashank-Rathor <shashankrathor180@gmail.com>
2023-05-23 10:54:17 +0530

    Updates

---
+++ precache-manifest.d2f2ce3a9da22475854e63db580cd86d.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d9721e11c30054fd732341d2fe63a9",
+    "url": "/wheelchairsolutions/index.html"
+  },
+  {
+    "revision": "56800d575e6326ca2434",
+    "url": "/wheelchairsolutions/static/css/2.a07e0b1d.chunk.css"
+  },
+  {
+    "revision": "ac790e1ac64ff0387797",
+    "url": "/wheelchairsolutions/static/css/main.8c9d80ca.chunk.css"
+  },
+  {
+    "revision": "56800d575e6326ca2434",
+    "url": "/wheelchairsolutions/static/js/2.e1b57b2d.chunk.js"
+  },
+  {
+    "revision": "c81277832004c904adf570ea254a3108",
+    "url": "/wheelchairsolutions/static/js/2.e1b57b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac790e1ac64ff0387797",
+    "url": "/wheelchairsolutions/static/js/main.d47681b8.chunk.js"
+  },
+  {
+    "revision": "fca938e189398adb7d0d",
+    "url": "/wheelchairsolutions/static/js/runtime-main.d109511b.js"
+  },
+  {
+    "revision": "7db9eb9b56cb3c7b4e48349f65f8e843",
+    "url": "/wheelchairsolutions/static/media/2.7db9eb9b.webp"
+  },
+  {
+    "revision": "0f63bde84db8bf853cea18d2451faf6d",
+    "url": "/wheelchairsolutions/static/media/3.0f63bde8.jpg"
+  },
+  {
+    "revision": "df2c05a432e5eda68641efb1e15439d2",
+    "url": "/wheelchairsolutions/static/media/6.df2c05a4.png"
+  },
+  {
+    "revision": "1ed547f1f0ca71189ae5f1006fd8a7cd",
+    "url": "/wheelchairsolutions/static/media/Active-wheelchair.1ed547f1.webp"
+  },
+  {
+    "revision": "2eebd74d8d73ddfced0c7e65105dfe4a",
+    "url": "/wheelchairsolutions/static/media/ContactUs.2eebd74d.png"
+  },
+  {
+    "revision": "6cef38592fab8068d614dceb70513efa",
+    "url": "/wheelchairsolutions/static/media/Electro Rhb-1.6cef3859.mp4"
+  },
+  {
+    "revision": "d256f8ed852d5f8846c34314a30386ab",
+    "url": "/wheelchairsolutions/static/media/FINALLOGO_BOLDED white text-01 (1).d256f8ed.png"
+  },
+  {
+    "revision": "a5e682646e17f6747328766fa43c1d3e",
+    "url": "/wheelchairsolutions/static/media/Flip Wheelchair website video.a5e68264.mp4"
+  },
+  {
+    "revision": "5f8c4839504b5afa007c76502c7dc604",
+    "url": "/wheelchairsolutions/static/media/Pediatric-Wheelchair.5f8c4839.webp"
+  },
+  {
+    "revision": "72ed766a50422294557044009fd4a3f6",
+    "url": "/wheelchairsolutions/static/media/Semi-active-Wheelchair.72ed766a.webp"
+  },
+  {
+    "revision": "0bbfc705e37a927ce2ae72b749b3154d",
+    "url": "/wheelchairsolutions/static/media/Simple-Line-Icons.0bbfc705.woff"
+  },
+  {
+    "revision": "6ac7320f709ffd2784b4a861e5b60395",
+    "url": "/wheelchairsolutions/static/media/Simple-Line-Icons.6ac7320f.woff2"
+  },
+  {
+    "revision": "b2892aa62b0fb2c21c8d7700e2ef6e56",
+    "url": "/wheelchairsolutions/static/media/Simple-Line-Icons.b2892aa6.ttf"
+  },
+  {
+    "revision": "dacd16c9a3d2cb61b472a35979015bf3",
+    "url": "/wheelchairsolutions/static/media/Simple-Line-Icons.dacd16c9.eot"
+  },
+  {
+    "revision": "de8ad55bfeaebbe67c5b9a362749de1a",
+    "url": "/wheelchairsolutions/static/media/Simple-Line-Icons.de8ad55b.svg"
+  },
+  {
+    "revision": "038651ef63447ec499665ddce97c002b",
+    "url": "/wheelchairsolutions/static/media/Sports-Wheelchair.038651ef.webp"
+  },
+  {
+    "revision": "61dc145361168e3df1988ac1c606f560",
+    "url": "/wheelchairsolutions/static/media/Transit-Wheelchair.61dc1453.webp"
+  },
+  {
+    "revision": "446a6e0258759b90b065cadbfc148d1e",
+    "url": "/wheelchairsolutions/static/media/back2.446a6e02.webp"
+  },
+  {
+    "revision": "33d0b67c047e466b46c464b8eefc2d31",
+    "url": "/wheelchairsolutions/static/media/custom-select.33d0b67c.png"
+  },
+  {
+    "revision": "f6dc099240cc766d0ba0fcacb7b1a74d",
+    "url": "/wheelchairsolutions/static/media/danger.f6dc0992.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wheelchairsolutions/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/wheelchairsolutions/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wheelchairsolutions/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wheelchairsolutions/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wheelchairsolutions/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7df769481095208e13c7c462eb3eb603",
+    "url": "/wheelchairsolutions/static/media/img0.7df76948.webp"
+  },
+  {
+    "revision": "0a6aeca363988a01dbc5d7b73655684d",
+    "url": "/wheelchairsolutions/static/media/img1.0a6aeca3.jpg"
+  },
+  {
+    "revision": "105fa2e1dfd0910c55d31fbeca5e4d3d",
+    "url": "/wheelchairsolutions/static/media/img1.105fa2e1.webp"
+  },
+  {
+    "revision": "48ea2cd4631126bf648e936a57bb1758",
+    "url": "/wheelchairsolutions/static/media/img1.48ea2cd4.webp"
+  },
+  {
+    "revision": "96bf3568951862e1a24a9a6cefd9f78a",
+    "url": "/wheelchairsolutions/static/media/img1.96bf3568.webp"
+  },
+  {
+    "revision": "f47c70edb2d84918855d348a571c0231",
+    "url": "/wheelchairsolutions/static/media/img1.f47c70ed.webp"
+  },
+  {
+    "revision": "40641bcfe62927e64b559a8b37ed91ba",
+    "url": "/wheelchairsolutions/static/media/img2.40641bcf.webp"
+  },
+  {
+    "revision": "8660aec9d16d87a38b02e5921a69374e",
+    "url": "/wheelchairsolutions/static/media/img2.8660aec9.jpg"
+  },
+  {
+    "revision": "f69b455fdab462ab6f5fcc1b126c78be",
+    "url": "/wheelchairsolutions/static/media/img2.f69b455f.jpg"
+  },
+  {
+    "revision": "97b925e1cbf5180244ebbe3e426089b9",
+    "url": "/wheelchairsolutions/static/media/img3.97b925e1.jpg"
+  },
+  {
+    "revision": "cc213822ec06cb4754dded41b25a7319",
+    "url": "/wheelchairsolutions/static/media/img3.cc213822.jfif"
+  },
+  {
+    "revision": "f2e5f82c3a4b3877786c3066b8388cbb",
+    "url": "/wheelchairsolutions/static/media/img3.f2e5f82c.webp"
+  },
+  {
+    "revision": "b718f318c0827496a2b1627c33838295",
+    "url": "/wheelchairsolutions/static/media/img4.b718f318.webp"
+  },
+  {
+    "revision": "d0aab86abbdfc4e8e24c93df626e5c5d",
+    "url": "/wheelchairsolutions/static/media/img4.d0aab86a.png"
+  },
+  {
+    "revision": "7815bbd0a691f0d3b66687777028777d",
+    "url": "/wheelchairsolutions/static/media/img5.7815bbd0.webp"
+  },
+  {
+    "revision": "794185886d7ce536a1d1b7ecb4df359a",
+    "url": "/wheelchairsolutions/static/media/img5.79418588.webp"
+  },
+  {
+    "revision": "80cfdc4084c7598bddb7d9b546464ce9",
+    "url": "/wheelchairsolutions/static/media/img6.80cfdc40.jpg"
+  },
+  {
+    "revision": "be30d3998f2ac4e20f5c9cadd657b0de",
+    "url": "/wheelchairsolutions/static/media/img6.be30d399.webp"
+  },
+  {
+    "revision": "2560be86a1679379f3a320fde44bd172",
+    "url": "/wheelchairsolutions/static/media/img7.2560be86.webp"
+  },
+  {
+    "revision": "a8418ff08822513634283bf59dd30c7b",
+    "url": "/wheelchairsolutions/static/media/img8.a8418ff0.webp"
+  },
+  {
+    "revision": "c0cb92c0aa0556261d071fd4f3f9eaa4",
+    "url": "/wheelchairsolutions/static/media/img8.c0cb92c0.webp"
+  },
+  {
+    "revision": "41ebd2a89b85c810e433172db3c23470",
+    "url": "/wheelchairsolutions/static/media/img9.41ebd2a8.jpg"
+  },
+  {
+    "revision": "001b6d9ea99d3c4e33f327174be4447e",
+    "url": "/wheelchairsolutions/static/media/logo.001b6d9e.png"
+  },
+  {
+    "revision": "31c19d109079ebcd9003821e4e4e01d7",
+    "url": "/wheelchairsolutions/static/media/logo2.31c19d10.png"
+  },
+  {
+    "revision": "bead829b50d91279b7d70e9fbdac7746",
+    "url": "/wheelchairsolutions/static/media/logo3.bead829b.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/wheelchairsolutions/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/wheelchairsolutions/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/wheelchairsolutions/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "de4ee7ad58f75b356c97fa17653567c7",
+    "url": "/wheelchairsolutions/static/media/success.de4ee7ad.svg"
+  },
+  {
+    "revision": "c94e6777c38431c93f2f88b187388875",
+    "url": "/wheelchairsolutions/static/media/t1.c94e6777.webp"
+  },
+  {
+    "revision": "72a3eba6f1e557559c590f0e5e832c2b",
+    "url": "/wheelchairsolutions/static/media/t2.72a3eba6.webp"
+  },
+  {
+    "revision": "eb49b0bcf37e2cbed080633a642e33e0",
+    "url": "/wheelchairsolutions/static/media/t3.eb49b0bc.webp"
+  },
+  {
+    "revision": "1678a7111cce754d9f398de78210033c",
+    "url": "/wheelchairsolutions/static/media/t4.1678a711.webp"
+  },
+  {
+    "revision": "b2de7b0a760119c55c1b8c1f0c8c318e",
+    "url": "/wheelchairsolutions/static/media/t5.b2de7b0a.webp"
+  },
+  {
+    "revision": "c3453248575b6e64163848f3bad79169",
+    "url": "/wheelchairsolutions/static/media/t6.c3453248.webp"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/wheelchairsolutions/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/wheelchairsolutions/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/wheelchairsolutions/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/wheelchairsolutions/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "7a3f95cec4b5847d44d1e08576dc921b",
+    "url": "/wheelchairsolutions/static/media/warning.7a3f95ce.svg"
+  }
+]);
